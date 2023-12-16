0f3b2016845fc85f6bd97a1a1c93cffef026aacc
Kirill <kirill.a.gribov@gmail.com>
2023-12-16 09:25:07 +0300

    рабочая версия

--- main.c
+++ main.c
@@ -9,7 +9,7 @@
 
 int main()
 {
-    const char* prompt_actions = "(a) in\n(b) out\n(c) sort\n";
+    const char* prompt_actions = "(a) in\n(b) out\n(c) sort\n(d) gen and test\n";
     const char* prompt_in = "(a) из стандартного потока ввода потока («с клавиатуры»);\n(b) из текстового файла (с возможностью указания имени файла);\n(c) из бинарного файла (с возможностью указания имени файла).\n";
     const char* prompt_out = "(a) в стандартный поток вывода («на экран»);\n(b) в текстовый файл (с возможностью указания имени файла);\n(c) в бинарный файл (с возможностью указания имени файла).\n";
     const char* prompt_sort = "(a) qsort\n(b) gnome_sort\n(c) insertion_sort\n";
@@ -18,9 +18,11 @@ int main()
     char state = 0, state_2 = 0, state_3 = 0;
     char* file_name = NULL;
     Node** nodes = NULL;
+    float time = 0;
     FILE* file = NULL;
     int len = 0, dir = 0;
     fptr_compare compare = NULL;
+    int sort = 0, cmp = 0, cnt = 0;
     printf("%s", prompt_actions);
     while (scanf("%c", &state) != EOF) {
         scanf("%*c");
@@ -131,26 +133,70 @@ int main()
                 return 0;
             }
             break;
+        case 'd':
+            printf("%s", prompt_sort);
+            if (scanf("%c", &state) != EOF) {
+                scanf("%*c");
+                printf("%s", prompt_cmp);
+                if (scanf("%c", &state_2) != EOF) {
+                    scanf("%*c");
+                    printf("%s", prompt_direction);
+                    if (scanf("%c", &state_3) != EOF) {
+                        scanf("%*c");
+                        printf("количества элементов в генерируемых массивах: ");
+                        if (scanf("%d", &len) != EOF) {
+                            printf("количества генерируемых массивов: ");
+                            if (scanf("%d", &cnt) != EOF) {
+                                scanf("%*c");
+                                switch (state_2) {
+                                case 'a':
+                                    cmp = NAME_MODE;
+                                    break;
+                                case 'b':
+                                    cmp = ID_MODE;
+                                    break;
+                                case 'c':
+                                    cmp = TIME_MODE;
+                                    break;
+                                }
+                                switch (state_3) {
+                                case 'a':
+                                    dir = DIRECT;
+                                    break;
+                                case 'b':
+                                    dir = REVERSE;
+                                    break;
+                                }
+                                switch (state) {
+                                case 'a':
+                                    sort = QSORT_MODE;
+                                    break;
+                                case 'b':
+                                    sort = GNOME_MODE;
+                                    break;
+                                case 'c':
+                                    sort = INSERTION_MODE;
+                                    break;
+                                }
+                                gen_arrays_nodes(&time, cnt, len, sort, cmp, dir);
+                                printf("avg time: %f\n", time);
+                            } else {
+                                return 0;
+                            }
+                        } else {
+                            return 0;
+                        }
+                    }
+                } else {
+                    return 0;
+                }
+            } else {
+                return 0;
+            }
+            break;
         }
         printf("%s", prompt_actions);
     }
     dealloc_nodes(nodes, len);
-    /*FILE* file_r = fopen("test.bin", "rb");
-    int cnt = 0;
-    Node** nodes = NULL;
-    if ((cnt = read(stdin, &nodes, FILE_MODE)) == 0) {
-        return 0;
-    }
-    FILE* file_w = fopen("test.txt", "w");
-    write(file_w, nodes, cnt, FILE_MODE);
-    write(stdout, nodes, cnt, FILE_MODE);
-    fclose(file_w);
-    fclose(file_r);
-    dealloc_nodes(nodes, cnt);*/
-
-    /*float time = 0;
-    int cnt = 10, len = 10000;
-    gen_arrays_nodes(&time, cnt, len, QSORT_MODE, NAME_MODE, REVERSE);
-    printf("%f", time);*/
     return 0;
 }
